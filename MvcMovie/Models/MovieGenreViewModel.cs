88df11b9ca88ffbc6803e243547458e776a5a637
kassem <kassem.s@bannernow.com>
2022-01-18 21:20:43 +0200

    Add project files.

---
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie>? Movies { get; set; }
+        public SelectList? Genres { get; set; }
+        public string? MovieGenre { get; set; }
+        public string? SearchString { get; set; }
+
+        //public static implicit operator MovieGenreViewModel(MovieGenreViewModel v)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+
+
+}
